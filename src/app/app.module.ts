581686ec4b0979ffe7acc3771c831be9f949e808
maqsoodahmad-1 <maqsodmaxood75@gmail.com>
2022-09-02 21:49:29 +0530

    Basic application created with angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { UserItemsComponent } from './user-items/user-items.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorldComponent,
+    UserItemsComponent,
+    UserListComponent,
   ],
   imports: [
     BrowserModule,
